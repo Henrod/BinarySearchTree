8d6dd179aa74a90e21b4acda7cb2b3964ac1f712
Henrique Rodrigues <henrique@localhost.localdomain>
2016-02-23 16:04:19 -0300

    No more bugs

--- src/bst.cpp
+++ src/bst.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 BST::BST() {
-	mRoot = std::unique_ptr<Node>(nullptr);
+	mRoot = unique_ptr<Node>(nullptr);
 }
 
 void BST::insert(int data) {
-	mRoot = std::move(insertNext(mRoot, data));
+	mRoot = move(insertNext(mRoot, data));
 }
 
-std::unique_ptr<Node>& BST::insertNext(std::unique_ptr<Node>& root, int data) {
+unique_ptr<Node>& BST::insertNext(unique_ptr<Node>& root, int data) {
 	if (root.get() == nullptr)
-		root = std::unique_ptr<Node>(new Node(data));
+		root = unique_ptr<Node>(new Node(data));
 	else if (data <= root->getData())
 		root->setLeft(insertNext(root->getLeft(), data));
 	else if (data >  root->getData())
@@ -27,14 +27,14 @@ void BST::printLevelOrder() {
 
 	for (int level = 0; level < mHeight; ++level) {
 		printGivenLevel(mRoot, level);
-		std::cout << std::endl;
+		cout << endl;
 	}
 }
-void 	BST::printGivenLevel	(const std::unique_ptr<Node>& root, int level) const {
+void 	BST::printGivenLevel	(const unique_ptr<Node>& root, int level) const {
 	if (root.get() == nullptr) {
-		std::cout << "-- ";
+		cout << "-- ";
 	} else if (level == 0) {
-		std::cout << root->getData() << " ";
+		cout << root->getData() << " ";
 	} else {
 		printGivenLevel(root->getLeft(), level - 1);
 		printGivenLevel(root->getRight(), level - 1);
@@ -44,7 +44,7 @@ void 	BST::printGivenLevel	(const std::unique_ptr<Node>& root, int level) const 
 int BST::getHeight() {
 	return getHeight(mRoot, 0);
 }
-int BST::getHeight	(const std::unique_ptr<Node>& root, int level) const {
+int BST::getHeight	(const unique_ptr<Node>& root, int level) const {
 	if (root.get() == nullptr) return level;
 
 	int n1 = getHeight(root->getLeft() , level + 1);
@@ -54,11 +54,11 @@ int BST::getHeight	(const std::unique_ptr<Node>& root, int level) const {
 }
 
 	bool BST::remove(int data) {
-		mRoot = std::move(remove(mRoot, data, mRoot));
+		mRoot = move(remove(mRoot, data, mRoot));
 		return mRoot != nullptr;
 	}
 
-	std::unique_ptr<Node>& BST::remove(std::unique_ptr<Node>& root, int data, std::unique_ptr<Node>& parent) {
+	unique_ptr<Node>& BST::remove(unique_ptr<Node>& root, int data, unique_ptr<Node>& parent) {
 		if (root.get() == nullptr) return root;
 		else if (data > root->getData()) remove(root->getRight(), data, root);
 		else if (data < root->getData()) remove(root->getLeft(), data, root);
@@ -66,39 +66,33 @@ int BST::getHeight	(const std::unique_ptr<Node>& root, int level) const {
 			/* No child */
 			if (root->getLeft().get() == nullptr && root->getRight().get() == nullptr) {
 				if (parent->getRight() == root) {
-					std::unique_ptr<Node> right (nullptr);
+					unique_ptr<Node> right (nullptr);
 					parent->setRight(right);
 				} else if (parent->getLeft() == root) {
-					std::unique_ptr<Node> left (nullptr);
+					unique_ptr<Node> left (nullptr);
 					parent->setLeft(left);
 				}
 
-				root = std::unique_ptr<Node>(nullptr);
+				root = unique_ptr<Node>(nullptr);
 			}
 				/* One child */
 		else if (root->getLeft().get() == nullptr)
 		{
-			std::unique_ptr<Node> tmp = std::move(root);
-
-			if (parent->getRight() == tmp) {
-				parent->setRight(tmp->getRight());
-			} else if (parent->getLeft() == tmp) {
-				parent->setLeft(tmp->getRight());
+			if (parent->getRight() == root) {
+				parent->setRight(root->getRight());
+			} else if (parent->getLeft() == root) {
+				parent->setLeft(root->getRight());
 			}
-
-			root = std::move(tmp->getRight());
+			root = move(root->getRight());
 		}
 		else if (root->getRight().get() == nullptr)
 		{
-			std::unique_ptr<Node> tmp = std::move(root);
-
-			if (parent->getRight() == tmp) {
-				parent->setRight(tmp->getLeft());
-			} else if (parent->getLeft() == tmp) {
-				parent->setLeft(tmp->getLeft());
+			if (parent->getRight() == root) {
+				parent->setRight(root->getLeft());
+			} else if (parent->getLeft() == root) {
+				parent->setLeft(root->getLeft());
 			}
-
-			root = std::move(tmp->getLeft());
+			root = move(root->getLeft());
 		}
 		/* Two childs */
 		else
@@ -113,7 +107,7 @@ int BST::getHeight	(const std::unique_ptr<Node>& root, int level) const {
 	return root;
 }
 
-Node* BST::getMin (const std::unique_ptr<Node>& root) const {
+Node* BST::getMin (const unique_ptr<Node>& root) const {
 	Node* min;
 	for (min = root.get(); min->getLeft() != nullptr; min = min->getLeft().get());
 
